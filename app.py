9ce59319aef868ed3c46936957778053d2ad850d
mani <manideepknv@gmail.com>
2025-07-21 15:52:09 +0530

    modified for production

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from gemini_client import rewrite_headline
+import os
 
 app = Flask(__name__)
 
@@ -18,5 +19,10 @@ def rewrite():
     neutral_headlines = rewrite_headline(headline)
     return jsonify({'neutral_headlines': neutral_headlines})
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+# if __name__ == '__main__':
+#     app.run(debug=True) 
+
+# for production
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
